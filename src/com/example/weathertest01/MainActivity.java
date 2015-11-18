bef6d2cd2611ac04ace9476ae254abc78f1e96d4
liufanglin <1085873616@qq.com>
2015-11-18 15:29:11 +0800

    用suorceTree修改后的代码

--- src/com/example/weathertest01/MainActivity.java
+++ src/com/example/weathertest01/MainActivity.java
@@ -48,6 +48,9 @@ public class MainActivity extends Activity {
 			}
 		}
 	};
+/**
+ * sourceTree ÐÞ¸Ä
+ */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
